309fa8dc3f70d97f7f7dcb77b4c50821ec658262
Swanand Deshpande <swananddeshpande121@gmail.com>
2023-06-18 23:33:46 +0530

    First Commit

---
+++ components/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import React, {useState, useEffect} from "react";
+import Image from 'next/image';
+import Link from 'next/link';
+
+// Icons
+import { MdNotifications } from 'react-icons/md'
+import { BsSearch } from 'react-icons/bs'
+import { CgMenuLeft, CgMenuRight } from 'react-icons/cg'
+
+// Internal Imports
+import { Discover, HelpCenter, Notification, Profile, SideBar } from './index'
+import { Button } from '../componentindex'  
+
+const NavBar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NavBar
---
+++ components/componentindex.js
@@ -0,0 +1,3 @@
+import Button from './Button'
+import Footer from './Footer'
+import NavBar from './NavBar/NavBar'
