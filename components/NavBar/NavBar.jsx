d1f45d83084251beccc976f3e999c7d499026ba1
Swanand Deshpande <swananddeshpande121@gmail.com>
2023-06-19 23:45:41 +0530

    Second Commit

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -11,13 +11,55 @@ import { CgMenuLeft, CgMenuRight } from 'react-icons/cg'
 // Internal Imports
 import { Discover, HelpCenter, Notification, Profile, SideBar } from './index'
 import { Button } from '../componentindex'  
+import images from '../../img'
 
 const NavBar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+
+    // =======USESTATE COMPONENTS=========
+
+    const [discover, setDiscover] = useState(false);
+    const [help, setHelp] = useState(false);
+    const [potification, setNotification] = useState(false);
+    const [profile, setProfile] = useState(false);
+    const [openSideMenu, setOpenSideMenu] = useState(false);
+
+    const openMenu = (e) => {
+        const btnText = e.target.innerText;
+        if(btnText == Discover){
+            setDiscover(true);
+            setHelp(false);
+            setNotification(false);
+            setProfile(false);
+        }
+    }
+
+    return (
+        <div className="navbar">
+            <div className="navbar_container">
+                <div className="navbar_container_left">
+                    <div className="logo">
+                        <Image src={images.logo} alt='NFT MARKET PLACE' height={100} width={100}/>
+                    </div>
+                    <div className="navbar_container_left_box_input">
+                        <div className="navbar_container_left_box_input_box">
+                            <input type="text" placeholder='Search NFT' />
+                            <BsSearch onClick={() =>{}} className='search_con'/>
+                        </div>
+                    </div>
+                </div>
+                
+                {/* END OF LEFT SECTION */}
+                <div className="navbar_container_right">
+                    <div className="navbar_container_right_discover">
+                        <p onClick={(e) => {}}>Discover</p>
+                        <div className="navbar_container_right_discover_box">
+                            <Discover/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
 
 export default NavBar
